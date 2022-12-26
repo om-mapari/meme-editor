f5656cec3ba1bf9c6f13372399fcc2c42d46664d
om mapari <mapariom05@gmail.com>
2022-12-26 10:54:10 +0530

    run deploy

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,7 +4,7 @@ export default function Header() {
     return (
         <header className="header">
             <img 
-                src="/images/troll-face.png"
+                src="https://i.ibb.co/V2wKfbh/troll-face.png"
                 className="header--image"
                 alt="troll-face"
             />
